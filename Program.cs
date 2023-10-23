1ca3d25fad9d9402cada90e7a0cad14c981b3e54
joseluiscontreras <joseluis.contreras@innocv.com>
2023-10-23 18:45:08 +0200

    IsEnvironment

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment()){
+if (app.Environment.IsDevelopment()
+|| app.Environment.IsStaging()
+|| app.Environment.IsEnvironment("Beta"))
+{
     app.UseDeveloperExceptionPage();
 }
 
